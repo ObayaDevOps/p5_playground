47343bc03ad943e2739eaff7aa46445c4fd68554
obi <obi@gmail>
2021-08-11 16:52:45 +0300

    adding print statements

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,10 +1,12 @@
 function setup() {
   // put setup code here
   createCanvas(400, 400);
+  print('setup !');
 }
 
 function draw() {
   // put drawing code here
   background(255,0,255);
   ellipse(200,200,50,50);
+  print('draw !');
 }

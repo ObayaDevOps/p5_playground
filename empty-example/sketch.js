e2d0d1e14d2a2a2389bc28f753f2dba64ee32f7e
obi <obi@gmail>
2021-09-16 17:44:19 +0300

    Attempt at Studio Lab Font

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,5 +1,25 @@
+var font;
+
+function preload() {
+  font = loadFont('DigiGroteskBoldCondensedRegular.otf');
+}
+
+
 function setup() {
   // put setup code here
+  createCanvas(800,800, WEBGL);
+  background(51);
+  
+  textFont(font);
+  textSize(128);
+  fill(255);
+  noStroke();
+  text('studioLab', 10, 100)
+
+
+  textSize (12);
+  text('inquiries:', 100, 500)
+
 }
 
 function draw() {

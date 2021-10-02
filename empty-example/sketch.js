47d11923fb204dae6faf277cdd1af46770bf250b
obi <obi@gmail>
2021-10-02 17:15:32 +0300

    One dimensional perlin noise circle

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,14 @@
+var xoff = 0; 
+
 function setup() {
   // put setup code here
+  createCanvas(400, 400);
 }
 
 function draw() {
   // put drawing code here
+  background(51);
+  var x = map(noise(xoff), 0, 1, 0, width);
+  xoff += 0.09;
+  ellipse(x,200, 24, 24);
 }
